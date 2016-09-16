8c454cfd9a763ffd59b3da75f8d7795807ba9a09
dbfowler <fowler.brady@gmail.com>
2016-09-16 14:29:42 -0400

    Updated exploratory script

--- Scripts/exploratory_analysis.R
+++ Scripts/exploratory_analysis.R
@@ -63,97 +63,121 @@ uber.x %>% filter(start_location_id!=end_location_id) %>% summarise(n())
 ## yep, we can probably drop it
 
 ####################################################################################
-## begin matching
+## begin matching uber data to crime data
 ####################################################################################
-setwd("/Users/bradyfowler/Documents/Fall Semester/Mining_6018/case1-crime/output/")
-load("uberx.Rda")
+## import latitude and longitude locations for each coordinate location in uber data
+  lat.long <- read.csv("/Users/bradyfowler/Documents/Fall Semester/Mining_6018/case1-crime/Data/awsLocations.csv")
+  #plot to see distribution
+    ggplot(lat.long, aes(x=longitude, y=latitude)) + geom_point() + coord_equal()
 
-## pull in latitude and longitude locations for each coordinate location
-lat.long <- read.csv("/Users/bradyfowler/Documents/Fall Semester/Mining_6018/case1-crime/Data/awsLocations.csv")
-#plot to see distribution
-ggplot(lat.long, aes(x=longitude, y=latitude)) + 
-  geom_point() + coord_equal()
+## import uberx data
+  setwd("/Users/bradyfowler/Documents/Fall Semester/Mining_6018/case1-crime/output/")
+  load("uberx.Rda")
 
-## join lat/long into uberx
+## join lat/long details into uberx
 ## remove unnecessary columns and cast the timestamps
-uber.x2  <- left_join(uber.x, lat.long, by=c("start_location_id"="locations")) %>% 
-  select(-end_location_id, -expected_wait_time, -product_type)
-uber.x2$timestamp <- ymd_hms(uber.x2$timestamp, tz = "UTC")
-head(uber.x2)
-
-## import crime data and fix column names
-crime <- read.csv("/Users/bradyfowler/Documents/Fall Semester/Mining_6018/case1-crime/Data/Crime_Incidents__2016.csv")
-colnames(crime)<-tolower(c("long", "lat", colnames(crime)[3:length(crime)]))
-nrow(crime)
-# 22,223
-
-# fix dates:
-crime$reportdatetime    <- ymd_hms(crime$reportdatetime, tz = "UTC")
-crime$lastmodifieddate  <- ymd_hms(crime$lastmodifieddate, tz = "UTC")
-crime$start_date        <- ymd_hms(crime$start_date, tz = "UTC")
-crime$end_date          <- ymd_hms(crime$end_date, tz = "UTC")
-
-## look at offense scatter across DC
-ggplot(crime, aes(x=long, y=lat)) + 
-  geom_point() + coord_equal()
-
-## look at offense type distribution
-crime %>% 
-  group_by(offense)  %>% 
-  summarise(count=n()) %>%
-  mutate(freq = count / sum(count)) %>% 
-  arrange(desc(count))
-  #                     offense count         freq
-  #                 THEFT/OTHER  8830 0.3973360932
-  #                THEFT F/AUTO  6615 0.2976645817
-  #                     ROBBERY  1966 0.0884669037
-  #         MOTOR VEHICLE THEFT  1614 0.0726274580
-  #  ASSAULT W/DANGEROUS WEAPON  1571 0.0706925258
-  #                    BURGLARY  1356 0.0610178644
-  #                   SEX ABUSE   181 0.0081447149
-  #                    HOMICIDE    86 0.0038698646
-  #                       ARSON     4 0.0001799937
-  # Probably only want to concentrate on non property crimes
-
-
-## create function to find the nearest crime (if any) to a surge price time
-## also make sure its pretty soon after
-
-# extract items of interest from crime table
-crime_list<- crime[,c("long", "lat", "reportdatetime", "objectid")]
-
-# standard city block is ~250 meters. we want to look within 4 blocks
-nearest_soonest <- function(row_num){
-  # details for current uber call
-  deets <- uber.x2[row_num, c("timestamp", "latitude", "longitude")]
-  # limit to crimes within an hour
-  potential_crimes <- crime_list[abs(difftime(deets[,1], crime_list$reportdatetime,units="hours")) < 1,]
-  # limit to crimes within 1000 meters
-  potential_crimes <- potential_crimes %>% 
-    mutate(dist = by(potential_crimes, 1:nrow(potential_crimes), function(row) { distm(c(deets[,2], deets[,3]), c(row$lat, row$long), fun = distHaversine)  })) %>% 
-    filter(dist<1000)
-  # return matched crimes
-  return(list(potential_crimes$objectid))
-}
+  uber.x2  <- left_join(uber.x, lat.long, by=c("start_location_id"="locations")) %>% 
+    select(-end_location_id, -expected_wait_time, -product_type)
+  uber.x2$timestamp <- ymd_hms(uber.x2$timestamp, tz = "UTC")
+  head(uber.x2)
 
 ## roll uber data up to the nearest hour
-uber.x2$timestamp.half <- uber.x2$timestamp
-minute(uber.x2$timestamp.half)<- floor(minute(uber.x2$timestamp)/30)*30
-second(uber.x2$timestamp.half)<-0
+  uber.x2$timestamp.half <- uber.x2$timestamp
+  minute(uber.x2$timestamp.half)<- floor(minute(uber.x2$timestamp)/30)*30
+  second(uber.x2$timestamp.half)<-0
 
-uber.x2.roll <- uber.x2 %>% 
-  dplyr::group_by(timestamp.half, start_location_id, latitude, longitude) %>% 
-  dplyr::summarise(avg.surge = mean(surge_multiplier, na.rm=TRUE),
-            avg.low   = mean(low_estimate, na.rm=TRUE),
-            avg.high  = mean(high_estimate, na.rm=TRUE)) 
+  uber.x2.roll <- uber.x2 %>% 
+    dplyr::group_by(timestamp.half, start_location_id, latitude, longitude) %>% 
+    dplyr::summarise(avg.surge = mean(surge_multiplier, na.rm=TRUE),
+                     avg.low   = mean(low_estimate, na.rm=TRUE),
+                     avg.high  = mean(high_estimate, na.rm=TRUE)) 
 
-# couldnt get the other sapply or mutate approach to work
-# so here ill apply it in a loop even if its not very eloquent
-for (i in 1:nrow(uber.x2.roll)) {
-  uber.x2.roll$dist[i] <- nearest_soonest(i) 
-}
+## import crime data and fix column names
+  crime <- read.csv("/Users/bradyfowler/Documents/Fall Semester/Mining_6018/case1-crime/Data/Crime_Incidents__2016.csv")
+  colnames(crime)<-tolower(c("long", "lat", colnames(crime)[3:length(crime)]))
+  nrow(crime)
+  # 22,223
 
+# fix dates:
+  crime$reportdatetime    <- ymd_hms(crime$reportdatetime, tz = "UTC")
+  crime$lastmodifieddate  <- ymd_hms(crime$lastmodifieddate, tz = "UTC")
+  crime$start_date        <- ymd_hms(crime$start_date, tz = "UTC")
+  crime$end_date          <- ymd_hms(crime$end_date, tz = "UTC")
 
+## check date overlap: crime data should be longer timeframe than uber:
+  crime %>% summarise(min(reportdatetime), max(reportdatetime))
+  uber.x2.roll %>% summarise(min(timestamp.half), max(timestamp.half))
+
+## limit crime data to be inside uber dates:
+  crime<-crime[crime$reportdatetime>min(uber.x2.roll$timestamp.half) &
+               crime$reportdatetime<max(uber.x2.roll$timestamp.half),]
+  nrow(crime)
+  # 2,228
+
+  ## look at offense scatter across DC
+  ggplot(crime, aes(x=long, y=lat, color=offense)) + geom_point() + coord_equal()
+
+  ## look at offense type distribution
+  crime %>% 
+    group_by(offense)  %>% 
+    dplyr::summarise(count=n()) %>%
+    mutate(freq = count / sum(count)) %>% 
+    arrange(desc(count))
+    #                     offense count         freq
+    #                THEFT/OTHER   868 0.3895870736
+    #               THEFT F/AUTO   719 0.3227109515
+    #                    ROBBERY   218 0.0978456014
+    # ASSAULT W/DANGEROUS WEAPON   147 0.0659784560
+    #        MOTOR VEHICLE THEFT   135 0.0605924596
+    #                   BURGLARY   114 0.0511669659
+    #                  SEX ABUSE    15 0.0067324955
+    #                   HOMICIDE    10 0.0044883303
+    #                      ARSON     2 0.0008976661
+    # Probably only want to concentrate on non property crimes
+
+# extract items of interest from crime table
+  crime_list<- crime[,c("long", "lat", "reportdatetime", "objectid")]
+
+######################################################################
+## find nearest uber point for each crime
+## use distm to create matrix of closest uber points
+## add closest uber point to crime table along with its distance
+######################################################################
+  for (i in 1:nrow(crime_list)) {
+    crime.point <- crime_list[i,1:2]
+    dists <- distm(crime.point, lat.long[,c("longitude","latitude")])
+    min_uber_dist <- which(dists==min(dists))
+    min_uber_point <- lat.long[,][min_uber_dist,]
+    crime_list$uber.lat[i] <- min_uber_point[,1] 
+    crime_list$uber.long[i] <- min_uber_point[,2] 
+    crime_list$uber.location[i] <- min_uber_point[,3] 
+    crime_list$uber.dist[i] <- min_uber_dist 
+  }
+
+  # whats the distribution of distances for the "nearest"
+  summary(crime_list$uber.dist)
+  hist(crime_list$uber.dist)
+  plot(ecdf(crime_list$uber.dist))
+  ## most crimes can be matched to a uber call within 200 meters ~80%
+
+######################################################################
+## find the uber call that is soonest to crime report at that point
+######################################################################
+  for (i in 1:nrow(crime_list)) {
+    # pull current crime details
+    crime.point <- crime_list[i,c("uber.location", "reportdatetime")]
+    # limit to crimes within an hour after
+    potential_ubers <- uber.x2.roll[uber.x2.roll$start_location_id==crime.point[,1],] %>% 
+      dplyr::mutate(diff = difftime(timestamp.half, crime.point[,2], units="hours")) %>% 
+      dplyr::filter(diff>0 & diff<=1) 
+    # select timestamp of soonest crime and assign to crime table
+    crime_list$uber.time[i] <- potential_ubers[potential_ubers$diff==min(potential_ubers$diff), c("timestamp.half")]
+    crime_list$uber.timediff[i]<-min(potential_ubers$diff)
+  }
+  crime_list$uber.time<-as.POSIXct(as.numeric(crime_list$uber.time), origin = "1970-01-01", tz = "UTC")
+  head(crime_list)
+ 
+  
 # look at clustering:
 library(ks);  library(RColorBrewer)
 points = crime[,c("X","Y")]
